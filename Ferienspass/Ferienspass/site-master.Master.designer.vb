9e95be222bf4508fe866175dfdf8977d5ed3430c
Alexander Reiter <alexander.reiter@htlvb.at>
2019-11-12 09:24:10 +0100

    Rename Ferienpass -> Ferienspass

---
+++ Ferienspass/Ferienspass/site-master.Master.designer.vb
@@ -0,0 +1,42 @@
+﻿'------------------------------------------------------------------------------
+' <automatisch generiert>
+'     Dieser Code wurde von einem Tool generiert.
+'
+'     Änderungen an dieser Datei können fehlerhaftes Verhalten verursachen und gehen verloren, wenn
+'     der Code neu generiert wird.
+' </automatisch generiert>
+'------------------------------------------------------------------------------
+
+Option Strict On
+Option Explicit On
+
+
+Partial Public Class site_master
+
+    '''<summary>
+    '''head-Steuerelement
+    '''</summary>
+    '''<remarks>
+    '''Automatisch generiertes Feld
+    '''Zum Ändern Felddeklaration aus der Designerdatei in eine Code-Behind-Datei verschieben.
+    '''</remarks>
+    Protected WithEvents head As Global.System.Web.UI.WebControls.ContentPlaceHolder
+
+    '''<summary>
+    '''form1-Steuerelement
+    '''</summary>
+    '''<remarks>
+    '''Automatisch generiertes Feld
+    '''Zum Ändern Felddeklaration aus der Designerdatei in eine Code-Behind-Datei verschieben.
+    '''</remarks>
+    Protected WithEvents form1 As Global.System.Web.UI.HtmlControls.HtmlForm
+
+    '''<summary>
+    '''ContentPlaceHolder1-Steuerelement
+    '''</summary>
+    '''<remarks>
+    '''Automatisch generiertes Feld
+    '''Zum Ändern Felddeklaration aus der Designerdatei in eine Code-Behind-Datei verschieben.
+    '''</remarks>
+    Protected WithEvents ContentPlaceHolder1 As Global.System.Web.UI.WebControls.ContentPlaceHolder
+End Class
