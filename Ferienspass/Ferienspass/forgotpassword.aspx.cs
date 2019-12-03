0b8afd62289a781920b8805c03caadb901e426ad
AndreasEIER <andreas.mair@htlvb.at>
2019-12-03 09:15:57 +0100

    pwForgotten hinzufügen

---
+++ Ferienspass/Ferienspass/forgotpassword.aspx.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+//Mair Andreas
+//03.12.19
+//Passwort vergessen + Passwort zurücksetzen
+
+namespace Ferienspass
+{
+    public partial class forgotpassword : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void btnGetNewPw_Click(object sender, EventArgs e)
+        {
+            if (AllTxtsFilled())
+            {
+                string sql = "SELECT * FROM user WHERE email=?";
+                string user = txtEmail.Text;
+
+                DB db = new DB();
+                DataTable sqlreturn = db.Query(sql, user);
+                if (sqlreturn.Rows.Count != 1)
+                {
+                    litEmailFailed.Text = "<div class='row'><div class='col'><div class='alert alert-danger'>PW zurücksetzen fehlgeschlagen!</div></div></div>";
+                }
+                else
+                {
+
+                    litEmailFailed.Text = "<div class='row'><div class='col'><div class='alert alert-success'>Erfolgreich! Überprüfen Sie Ihr Postfach!</div></div></div>";
+                }
+            }
+            else
+            {
+                litEmailFailed.Text = "<div class='row'><div class='col'><div class='alert alert-danger'>Bitte geben Sie eine EmailAdresse ein!</div></div></div>";
+            }
+
+        }
+
+        private bool AllTxtsFilled()
+        {
+            if (string.IsNullOrEmpty(txtEmail.Text)) return false;
+            return true;
+        }
+    }
+}
---
+++ Ferienspass/Ferienspass/forgotpassword.aspx.designer.cs
@@ -0,0 +1,53 @@
+﻿//------------------------------------------------------------------------------
+// <automatisch generiert>
+//     Dieser Code wurde von einem Tool generiert.
+//
+//     Änderungen an dieser Datei können fehlerhaftes Verhalten verursachen und gehen verloren, wenn
+//     der Code neu generiert wird.
+// </automatisch generiert>
+//------------------------------------------------------------------------------
+
+namespace Ferienspass
+{
+
+
+    public partial class forgotpassword
+    {
+
+        /// <summary>
+        /// form1-Steuerelement
+        /// </summary>
+        /// <remarks>
+        /// Automatisch generiertes Feld
+        /// Zum Ändern Felddeklaration aus der Designerdatei in eine Code-Behind-Datei verschieben.
+        /// </remarks>
+        protected global::System.Web.UI.HtmlControls.HtmlForm form1;
+
+        /// <summary>
+        /// litEmailFailed-Steuerelement
+        /// </summary>
+        /// <remarks>
+        /// Automatisch generiertes Feld
+        /// Zum Ändern Felddeklaration aus der Designerdatei in eine Code-Behind-Datei verschieben.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Literal litEmailFailed;
+
+        /// <summary>
+        /// txtEmail-Steuerelement
+        /// </summary>
+        /// <remarks>
+        /// Automatisch generiertes Feld
+        /// Zum Ändern Felddeklaration aus der Designerdatei in eine Code-Behind-Datei verschieben.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox txtEmail;
+
+        /// <summary>
+        /// btnGetNewPw-Steuerelement
+        /// </summary>
+        /// <remarks>
+        /// Automatisch generiertes Feld
+        /// Zum Ändern Felddeklaration aus der Designerdatei in eine Code-Behind-Datei verschieben.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Button btnGetNewPw;
+    }
+}
