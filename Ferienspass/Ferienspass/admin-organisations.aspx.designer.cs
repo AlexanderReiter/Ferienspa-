1300b0763a1b7d5f38e482278e24915b781e2459
Marcel Kollross <marcel.kollross@gmail.com>
2020-03-03 07:52:37 +0100

    Organisationen

--- Ferienspass/Ferienspass/admin-organisations.aspx.designer.cs
+++ Ferienspass/Ferienspass/admin-organisations.aspx.designer.cs
@@ -10,33 +10,24 @@
 namespace Ferienspass {
     
     
-    public partial class admin_master {
+    public partial class admin_organisations {
         
         /// <summary>
-        /// head control.
+        /// litError control.
         /// </summary>
         /// <remarks>
         /// Auto-generated field.
         /// To modify move field declaration from designer file to code-behind file.
         /// </remarks>
-        protected global::System.Web.UI.WebControls.ContentPlaceHolder head;
+        protected global::System.Web.UI.WebControls.Literal litError;
         
         /// <summary>
-        /// form1 control.
+        /// gvOrganisations control.
         /// </summary>
         /// <remarks>
         /// Auto-generated field.
         /// To modify move field declaration from designer file to code-behind file.
         /// </remarks>
-        protected global::System.Web.UI.HtmlControls.HtmlForm form1;
-        
-        /// <summary>
-        /// ContentPlaceHolder1 control.
-        /// </summary>
-        /// <remarks>
-        /// Auto-generated field.
-        /// To modify move field declaration from designer file to code-behind file.
-        /// </remarks>
-        protected global::System.Web.UI.WebControls.ContentPlaceHolder ContentPlaceHolder1;
+        protected global::System.Web.UI.WebControls.GridView gvOrganisations;
     }
 }
